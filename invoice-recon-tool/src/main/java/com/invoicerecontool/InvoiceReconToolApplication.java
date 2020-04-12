f5d21740d99cf00d979ee0b17c07937b449ea542
ptk23 <prtkinnovation@gmail.com>
2020-04-12 22:28:04 +0530

    First commit

---
+++ invoice-recon-tool/src/main/java/com/invoicerecontool/InvoiceReconToolApplication.java
@@ -0,0 +1,13 @@
+package com.invoicerecontool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceReconToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceReconToolApplication.class, args);
+	}
+
+}
---
+++ invoice-recon-tool/src/test/java/com/invoicerecontool/InvoiceReconToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoicerecontool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceReconToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
